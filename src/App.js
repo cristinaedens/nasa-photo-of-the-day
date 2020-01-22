e92747c71d1d75f77baf97dffe3b966f8f8ff8af
Cristina Edens <edens.cristina@gmail.com>
2020-01-22 15:38:29 -0500

    Pulled in Data from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+
+  const [data, setdata]= useState([]);
+  
+
+  useEffect(()=> {
+    axios.get(`https://api.nasa.gov/planetary/apod?date=${"2020-01-22"}&api_key=o5SHwwrXZXk6O4JRf5m4dAosTNLzeum3aMJ3ylmt`)
+    .then(response =>{
+      console.log(response)
+    })
+    .catch(error =>{
+      console.log(error)
+    })
+  }, [])
   return (
     <div className="App">
       <p>
