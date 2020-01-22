b6933196f495da1092c88b53aaeefa3ee5c955d4
Cristina Edens <edens.cristina@gmail.com>
2020-01-22 17:46:40 -0500

    added alt tag to img

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,7 @@ function Card (props) {
   return(
     <div className="card">
       <h1>{props.data.title}</h1>
-      <img src= {props.data.hdurl} />
+      <img alt={props.data.title} src= {props.data.hdurl} />
       <h3>{props.data.date}</h3>
       <p>{props.data.explanation}</p>
   <p>{props.data.copyright}</p>
